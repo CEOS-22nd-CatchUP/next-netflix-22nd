a5c28984fd55db0f38611aa32a27ce3a52bcaf15
yam <1669jsh@naver.com>
2025-10-31 04:05:38 +0900

    feat: api 호출해 현재 영화 이미지 추가 구현

--- src/components/CurrentPlaying.tsx
+++ src/components/CurrentPlaying.tsx
@@ -1,9 +1,32 @@
-const CurrentPlaying = () => {
+import Image from "next/image";
+import top10Logo from '@/svgs/menuBar/top10Logo.svg'
+import { BIG_IMAGE_BASE_URL, getPopularMovies} from "@/api/tmdb";
+
+const CurrentPlaying = async() => {
+    const movies = await getPopularMovies();
+
+    const top10Movies = movies.slice(0, 10);
+
+    const randomIndex = Math.floor(Math.random() * top10Movies.length);
+    const movie = top10Movies[randomIndex];
+    const rank = randomIndex + 1;
+
     return(
-        <div className="flex flex-col justify-between items-center border w-full h-[415px]">
+        <div className="flex flex-col justify-between items-center w-full h-[415px]">
+            <Image
+            src={`${BIG_IMAGE_BASE_URL}${movie.backdrop_path}`}
+            alt={movie.title}
+            width={1920}
+            height={415}
+            className="w-full h-[415px] object-cover brightness- z-0"
+            />
+
             {/* backgroundIMG */}
             <div className="justify-center items-center"></div>  
-            <div className="relative z-10">#2 in Nigeria Today</div>
+            <div className="flex relative z-10 text-title2-sb">
+                <Image src={top10Logo} alt="top10" className="mr-1"/>
+                #{rank} in Korea Today
+                </div>
         </div>
     );
 }
