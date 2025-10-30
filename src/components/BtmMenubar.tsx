12e617b01801e8e2dc80dcb4b6ae5ee335150787
yam <1669jsh@naver.com>
2025-10-31 04:04:22 +0900

    design: 메뉴바 css 수정

--- src/components/BtmMenubar.tsx
+++ src/components/BtmMenubar.tsx
@@ -1,7 +1,7 @@
 const BtmMenubar = () => {
     return(
         // 일단 모바일 부분 btm 살짝 띄워놨어!
-        <div className="w-[375px] h-20 pb-[31.7px] px-5 gap-[10] justify-between border absolute bottom-0 flex flex-end">
+        <div className="w-[375px] h-20 pb-[31.7px] px-5 gap-[10] justify-between border absolute bottom-0 flex flex-end bg-gray-3 text-gray-1 text-nav3-sm">
             <div className="border">Home</div>
             <div className="border">Search</div>
             <div className="border">Comming Soon</div>
--- src/components/MidMenubar.tsx
+++ src/components/MidMenubar.tsx
@@ -1,9 +1,24 @@
+import AddBtn from '@/svgs/previewBar/addButton.svg';
+import PlayBtn from '@/svgs/previewBar/playButton.svg';
+import InfoBtn from '@/svgs/previewBar/info.svg';
+import Image from 'next/image';
+
 const MidMenubar = () => {
     return(
-        <div className="flex w-[259px] h-[45px] mt-[5] border items-center justify-between">
-            <div className="">My List</div>
-            <div className="">Play</div>
-            <div className="">Info</div>
+        <div className="flex w-[259px] h-[45px] mt-[8] items-center justify-between">
+            <div className="flex flex-col justify-center items-center text-nav2-sm text-white">
+               <Image src={AddBtn} alt='add my list'/>
+                <p>My List</p>
+                </div>
+            <div className="flex items-center justify-between w-[110px] h-[45px] bg-gray-0 hover:bg-white rounded-[5px] text-title1-sb text-black">
+                <Image src={PlayBtn} alt='play' className='ml-[19px]'/>
+                <p className='mr-[19px]'>Play</p>
+                </div> 
+            <div className="flex flex-col justify-center items-center text-nav2-sm text-white">
+                <Image src={InfoBtn} alt='info'/>
+                <p>Info
+                    </p>
+                </div>
         </div>
     )
 
