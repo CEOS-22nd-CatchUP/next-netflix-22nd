b4abd0b1e2d383a82d4bdd548d34c19fafd72f8b
yam <1669jsh@naver.com>
2025-10-31 04:05:05 +0900

    style: 기타 수정

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -3,16 +3,19 @@ import MidMenubar from '@/components/MidMenubar';
 import Previews from '@/components/Previews';
 import TheOthers from '@/components/TheOthers';
 
-export default function Page(){
+export default async function Page(){
     return(
-        <div className="flex flex-col">
+        <div className="flex flex-col bg-black text-white">
             <CurrentPlaying />
             <div className="flex justify-center">
                 <MidMenubar />
             </div>
             <Previews />
-                        <Previews />
             <TheOthers />
+            <TheOthers />
+            <TheOthers />
+            <TheOthers />
+            <div className="h-20" />
         </div>
     )
 }
