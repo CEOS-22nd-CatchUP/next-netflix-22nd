4250395807ab7891609f4f5b62bdc9c3953d691c
yam <1669jsh@naver.com>
2025-10-31 04:03:35 +0900

    design: TopMenubar css 수정

--- src/components/TopMenubar.tsx
+++ src/components/TopMenubar.tsx
@@ -1,11 +1,16 @@
+import Logo from '@/svgs/logo/smallLogo.svg';
+import Image from 'next/image';
+
 const TopMenubar = () => {
     return(
-        <div className="border flex mt-6 w-[338px] h-[57px] gap-6">
-            <div className="border">Logo</div>
-            <div className="flex justify-between w-[280px]">
-                <div className="border">TV Shows</div>
-                <div className="border">Movies</div>
-                <div className="border">My List</div>
+        <div className=" flex items-center mt-4 w-[338px] h-[57px] gap-6 text-white">
+            <div className="">
+                <Image src={Logo} alt='netflix'/>
+            </div>
+            <div className="flex justify-between w-[280px] text-nav1-sm">
+                <div className="">TV Shows</div>
+                <div className="">Movies</div>
+                <div className="">My List</div>
             </div>
         </div>
     )
