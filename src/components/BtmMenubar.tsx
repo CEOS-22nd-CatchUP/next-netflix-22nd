113c42eae09eb3f16bb8283baa22a1be342719a6
jy000n <jayoonchang@naver.com>
2025-11-01 05:06:15 +0900

    feat: BtmMenubar 클릭 UI

--- src/components/BtmMenubar.tsx
+++ src/components/BtmMenubar.tsx
@@ -1,17 +1,24 @@
+'use client';
+
+import {useState} from 'react';
 import { BTM_MENU_ITEMS } from "@/constants/menuItems";
 
 const BtmMenubar = () => {
+    const [active, setActive] = useState(0);
+
     return(
-        <div className="py-3 gap-[7px] w-[375px] justify-center flex absolute bottom-0 items-center bg-gray-3 text-gray-1 text-nav3-sm">
-            {BTM_MENU_ITEMS.map((item) => {
+        <div className="py-3 gap-[7px] w-[375px] justify-center flex absolute bottom-0 items-center bg-gray-3 text-nav3-sm">
+            {BTM_MENU_ITEMS.map((item, index) => {
                 const Icon = item.icon;
+                const isActive = index === active;
+
                 return(
-                    <div key={item.label} className="w-[63.8px] h-10 items-center">
+                    <div key={item.label} className="w-[63.8px] h-10 items-center" onClick={()=>setActive(index)}>
                     <div className={`flex flex-col items-center ${item.adjustTop ? '-mt-px' : ''}`}>
                         <div className="cursor-pointer">
-                        <Icon />
+                        <Icon color={isActive ? '#ffffff' : '#8C8787'}/>
                         </div>
-                        <p className={`${item.mtClass} cursor-pointer`}>{item.label}</p>
+                        <p className={`${item.mtClass} ${isActive ? 'text-white' : 'text-gray-1'} cursor-pointer`}>{item.label}</p>
                     </div>
                     </div>
                 );
--- src/components/icons/HomeIcon.tsx
+++ src/components/icons/HomeIcon.tsx
@@ -1,12 +1,12 @@
 import { IconProps } from '@/type/menu.type';
 
-const HomeIcon = ({ stroke="#8C8787", fill="none", ...props}: IconProps) => (
+const HomeIcon = ({ color="#8C8787", fill="none", ...props}: IconProps) => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill={fill} xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M22 9.15789L12 2" stroke={stroke} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M2 9.15789L12 2" stroke={stroke} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
-    <path d="M5 19V7.36841" stroke={stroke} stroke-width="2" stroke-linecap="round"/>
-    <path d="M19 19V7.36841" stroke={stroke} stroke-width="2" stroke-linecap="round"/>
-    <path d="M5 19H19" stroke={stroke} stroke-width="2" stroke-linecap="round"/>
+    <path d="M22 9.15789L12 2" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M2 9.15789L12 2" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
+    <path d="M5 19V7.36841" stroke={color} stroke-width="2" stroke-linecap="round"/>
+    <path d="M19 19V7.36841" stroke={color} stroke-width="2" stroke-linecap="round"/>
+    <path d="M5 19H19" stroke={color} stroke-width="2" stroke-linecap="round"/>
     </svg>
 );
 
