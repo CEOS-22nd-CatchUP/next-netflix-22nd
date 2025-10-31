0ba6d505187643b1c2c39d7b8191d4782f101f7b
jy000n <jayoonchang@naver.com>
2025-11-01 04:56:52 +0900

    fix: TheOthers.tsx 파일 async() 관련 에러로 인한 주석처리+코드수정

--- src/components/TheOthers.tsx
+++ src/components/TheOthers.tsx
@@ -1,21 +1,37 @@
 import { getPopularMovies } from "@/api/tmdb";
 import { RectangleSliders } from "./carousel/Carousel";
 
-const TheOthers =async () => {
-    const movies = await getPopularMovies();
+// const TheOthers =async () => {
+//     const movies = await getPopularMovies();
 
-    return(
-        <div className="mt-7">
-            <div className="ml-4 w-[200px] h-26.75px">
-                <p className="text-title4-sb">
-                    Trending Now
-                    </p>
-                    </div>
-            <div className="ml-4 mt-2.5 flex gap-2">
-               <RectangleSliders movies={movies}/>
-            </div>
-        </div>
-    )
+//     return(
+//         <div className="mt-7">
+//             <div className="ml-4 w-[200px] h-26.75px">
+//                 <p className="text-title4-sb">
+//                     Trending Now
+//                     </p>
+//                     </div>
+//             <div className="ml-4 mt-2.5 flex gap-2">
+//                <RectangleSliders movies={movies}/>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default TheOthers;
+
+// async function 선언
+export default async function TheOthers() {
+  const movies = await getPopularMovies();
+
+  return (
+    <div className="mt-7">
+      <div className="ml-4 w-[200px] h-26.75px">
+        <p className="text-title4-sb">Trending Now</p>
+      </div>
+      <div className="ml-4 mt-2.5 flex gap-2">
+        <RectangleSliders movies={movies} />
+      </div>
+    </div>
+  );
 }
-
-export default TheOthers;
