661c4aba4d74cce5c085d5955eb3aedba1ba7083
jy000n <jayoonchang@naver.com>
2025-11-01 23:31:39 +0900

    feat: MidNavbar 텍스트 추가

--- src/components/MidNavbar.tsx
+++ src/components/MidNavbar.tsx
@@ -9,6 +9,7 @@ const MidNavbar = () => {
     <div className="mt-2 flex h-[45px] w-[259px] items-center justify-between">
       <div className="text-nav2-sm flex cursor-pointer flex-col items-center justify-center text-white">
         <AddBtn />
+        <p>Add</p>
       </div>
       <div className="bg-gray-0 text-title1-sb flex h-[45px] w-[110px] cursor-pointer items-center justify-between rounded-[5px] text-black hover:bg-white">
         <PlayBtn className="ml-[19px]" />
