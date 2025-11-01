c10f4035c7736974d5d0ee8bcc9fdccbd554bf5b
jy000n <jayoonchang@naver.com>
2025-11-01 16:41:21 +0900

    style: 메뉴바 px 고정 해제

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -3,8 +3,8 @@ import BtmMenubar from '@/components/BtmMenubar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen w-screen items-center justify-center border">
-      <div className="relative flex h-[812px] w-[375px] justify-center overflow-hidden border">
+    <div className="flex h-screen w-screen items-center justify-center bg-black">
+      <div className="relative flex h-full w-[375px] justify-center overflow-hidden">
         <div className="absolute top-0 z-20 flex">
           <TopMenubar />
         </div>
--- src/components/TopMenubar.tsx
+++ src/components/TopMenubar.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 
 const TopMenubar = () => {
   return (
-    <div className="mt-4 flex h-[57px] w-[338px] items-center gap-6 text-white">
-      <div className="">
+    <div className="mt-4 flex h-[57px] items-center">
+      <div className="mr-5 -ml-6">
         <Image src={Logo} alt="netflix" />
       </div>
-      <div className="text-nav1-sm flex w-[280px] justify-between">
+      <div className="text-nav1-sm flex justify-center gap-8 text-white">
         <div className="">TV Shows</div>
         <div className="">Movies</div>
         <div className="">My List</div>
