78e1a971a86230b80b79d304c35e6c06414e7d83
yam <1669jsh@naver.com>
2025-11-08 12:56:49 +0900

    feat: 검색 input 구현

--- src/components/search/InputSearch.tsx
+++ src/components/search/InputSearch.tsx
@@ -1,8 +1,26 @@
+'use client';
 import SearchIcon from '@/assets/svgs/search/searchIcon.svg?url';
 import XIcon from '@/components/search/icons/XIcon';
+import { useSearchStore } from '@/store/useSearchStore';
 import Image from 'next/image';
+import { useEffect } from 'react';
 
-const InputSearch = () => {
+export default function InputSearch() {
+  const { query, setQuery, search } = useSearchStore();
+
+  const handleClear = () => {
+    setQuery('');
+  };
+
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      if (query.trim()) {
+        search();
+      }
+    }, 500);
+    return () => clearTimeout(debounceTimer);
+  }, [query, search]);
+
   return (
     <>
       <div className="bg-gray-2 flex h-[52px] w-full">
@@ -11,12 +29,12 @@ const InputSearch = () => {
           <input
             placeholder="Search for a show, movie, genre, e.t.c"
             className="placeholder-gray-0 text-text3-sb mx-2 flex-1 text-white caret-white focus:outline-none"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
           />
-          <XIcon className="text-gray-0 cursor-pointer hover:text-white" />
+          <XIcon className="text-gray-0 cursor-pointer hover:text-white" onClick={handleClear} />
         </div>
       </div>
     </>
   );
-};
-
-export default InputSearch;
+}
